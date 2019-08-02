513dfd1c3024525a2cac7555a819680b7b9cdf95
Dominick Del Bosque <dominick@theosfp.com>
2019-08-02 07:02:07 -0700

    Modeled ESUP data and created default index view.

--- src/controllers/application_controller.rb
+++ src/controllers/application_controller.rb
@@ -6,34 +6,26 @@ require 'sinatra'
 Dotenv.load('.env')
 
 class ApplicationController < Sinatra::Base
+	configure do
+  	set :views, "src/views"
+  end
+
 	options = {
 	  :username => ENV['USERNAME'],
 	  :password => ENV['API_TOKEN'],
-	  :site     => 'https://supsupport.atlassian.net', # or 'https://<your_subdomain>.atlassian.net'
+	  :site     => 'https://demandbase.atlassian.net', # or 'https://<your_subdomain>.atlassian.net'
 	  :context_path => '', # often blank
 	  :auth_type => :basic,
 	  :read_timeout => 120
 	}
 
 	client = JIRA::Client.new(options)
+	esup = client.Issue.jql('PROJECT = "ESUP"').each do |issue|
+		pp issue.fields
+	end
 
-	# List All Projects
-	# projects = client.Project.all
-	# projects.each do |project|
-	# 	pp project
-	# end
-
-	# GET All Issues
-	issues = client.Issue.all
-
-	# List All Issues
-	issues.each do |issue|
-		puts "#{issue.id} - #{issue.key}"
-		puts "Priority - #{issue.priority.id}"
-		puts "#{issue.reporter.displayName}"
-		puts "Created - #{Date.parse(issue.created).strftime('%m/%d/%Y')}"
-		puts "Updated - #{Date.parse(issue.updated).strftime('%m/%d/%Y')}"
-		puts "Last Viewed - #{Date.parse(issue.lastViewed).strftime('%m/%d/%Y')}"
-		puts
+	get '/' do
+		@esup = client.Project.find("ESUP")
+		erb :index
 	end
 end
