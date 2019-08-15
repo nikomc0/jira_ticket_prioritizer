bfb51a55657dfe77cf14f137a7a04dc8ff35c3aa
Dominick Del Bosque <dominick@theosfp.com>
2019-08-15 07:53:04 -0700

    code clean up for Issue Controller

--- src/controllers/issue_controller.rb
+++ src/controllers/issue_controller.rb
@@ -7,14 +7,6 @@ require_relative '../services/prioritizer'
 
 Dotenv.load('.env')
 
-# module TicketPrioritizer
-	# include Prioritizer
-
-	# def self.tickets(args)
-	# 	IssueService.new(args[:tickets]).get_tickets
-	# end
-# end
-
 class IssueController < Sinatra::Base
 	extend TicketPrioritizer
 
@@ -24,13 +16,13 @@ class IssueController < Sinatra::Base
   end
 
 	options = {
-		  :username => ENV['USERNAME'],
-		  :password => ENV['API_TOKEN'],
-		  :site     => 'https://demandbase.atlassian.net', # or 'https://<your_subdomain>.atlassian.net'
-		  :context_path => '', # often blank
-		  :auth_type => :basic,
-		  :read_timeout => 120
-		}
+		:username => ENV['USERNAME'],
+		:password => ENV['API_TOKEN'],
+		:site     => 'https://demandbase.atlassian.net', # or 'https://<your_subdomain>.atlassian.net'
+		:context_path => '', # often blank
+		:auth_type => :basic,
+		:read_timeout => 120
+	}
 
 	
 	@@client = JIRA::Client.new(options)
@@ -38,17 +30,6 @@ class IssueController < Sinatra::Base
 	esups = @@client.Issue.jql('PROJECT = "ESUP" AND ISSUETYPE in ("Bug", "Task") AND CREATOR in ("ddelbosque", "balbini", "jluse", "apaley") AND STATUS in ("new", "In Progress", "acknowledge")', fields:[:status, :summary, :priority, :issuetype, :created, :updated, :lastViewed, :assignee, :creator], max_results: 1000)
 	
 	@@prioritizer = TicketPrioritizer::Prioritizer.new(esups)
-
-	# pp @esups[0]
-
-	# def esups
-	# 	esups = @@client.Issue.jql('PROJECT = "ESUP" AND ISSUETYPE in ("Bug") AND CREATOR in ("klange", "ddelbosque", "balbini", "jluse", "apaley") AND STATUS in ("new", "In Progress", "acknowledge")', fields:[:status, :summary, :priority, :issuetype, :created, :updated, :lastViewed, :assignee, :creator], max_results: 1000)
-	# 	priority_tickets = Prioritizer.new(esups).get_tickets
-	# end
-
-	# def array
-	# 	puts 'worked'
-	# end
 
 	def priority_tickets
 		@@prioritizer.get_tickets
